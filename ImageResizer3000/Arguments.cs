d1018b82f5bca8c794c946486647b764fa009083
Eidamiino <adam.pala199@gmail.com>
2022-07-08 18:46:57 +0200

    how does c# work again

---
+++ ImageResizer3000/Arguments.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace ImageResizer3000
+{
+	internal class Arguments
+	{
+		public string dirPath;
+		public string command;
+		public string width;
+
+		public Arguments(string[] argsFromMain)
+		{
+			if (argsFromMain.Length < 2)
+			{
+				Console.WriteLine("Invalid args!");
+				return;
+			}
+			DirPath = argsFromMain[0];
+			command = argsFromMain[1];
+			width = argsFromMain[2];
+		}
+		public string DirPath
+		{
+			get { return dirPath; }
+			private set
+			{
+				if (!DirExists(dirPath))
+					throw new Exception("Error");
+				dirPath = value;
+			}
+		}
+		public static bool DirExists(string dirPath)
+		{
+			if (!Directory.Exists(dirPath))
+			{
+				Console.WriteLine($"Directory with path {dirPath} does not exist");
+				return false;
+			}
+
+			return true;
+		}
+	}
+}
--- ImageResizer3000/Program.cs
+++ ImageResizer3000/Program.cs
@@ -1,12 +1,66 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
 
 namespace ImageResizer3000
 {
 	internal class Program
 	{
+		//konstantni list hledanych pripon
+		public const string SearchedExtensions = "*.jpg";
+
 		static void Main(string[] args)
+
 		{
-			Console.WriteLine("Hello World!");
+
+
+			var path = args[0];
+			var command = args[1];
+
+			switch (command)
+			{
+				case "-r":
+				case "--resize":
+				{
+					var allImagesPath = Directory.GetFiles(path, SearchedExtensions);
+					if (allImagesPath.Length == 0)
+					{
+						Console.WriteLine("Entered directory does not contain any jpg/jpeg files to resize");
+						return;
+					}
+
+					foreach (var imagePath in allImagesPath)
+					{
+						string width = args[2].Substring(3);
+
+						using Image image = Image.Load(imagePath);
+						image.Mutate(x => x.Resize(0,int.Parse(width)));
+						string outPath = imagePath.Insert(imagePath.IndexOf('.'), $".{width}");
+						image.Save(outPath);
+						Console.WriteLine($"Image {imagePath.Substring(0,imagePath.IndexOf('.'))} resized in ms");
+
+
+					}
+				}
+					break;
+			}
+
+			{
+			}
 		}
+
+
+		private static string ReadValue(string label, string defaultValue)
+		{
+			Console.Write($"{label} (default: {defaultValue}): ");
+			string value = Console.ReadLine();
+			if (value == "")
+				return defaultValue;
+			return value;
+		}
+
+		
 	}
 }
